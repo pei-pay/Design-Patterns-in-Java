a2a4bdb089c93c5bd0a32273ba8b1ee17f44d181
pei-pay <gleektennis@icloud.com>
2022-08-06 23:28:14 +0900

    017: Observer IncrementalNumberGenerator

---
+++ observer/Q1/Main.java
@@ -0,0 +1,18 @@
+package observer.Q1;
+import observer.Observer;
+import observer.DigitObserver;
+import observer.GraphObserver;
+import observer.NumberGenerator;
+
+public class Main {
+  public static void main(String[] args) {
+    NumberGenerator generator = new IncrementalNumberGenerator(10, 50, 5);
+
+    Observer observer1 = new DigitObserver();
+    Observer observer2 = new GraphObserver();
+
+    generator.addObserver(observer1);
+    generator.addObserver(observer2);
+    generator.execute();
+  }
+}
