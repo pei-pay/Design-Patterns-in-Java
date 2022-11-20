56ca1439f7840f53b35e28914ec1032759c92b39
pei-pay <gleektennis@icloud.com>
2022-11-20 15:30:58 +0900

    add: prototype

---
+++ prototype/Main.java
@@ -0,0 +1,20 @@
+package prototype;
+
+public class Main {
+  public static void main(String[] args) {
+    Manager manager = new Manager();
+    UnderlinePen uPen = new UnderlinePen('~');
+    MessageBox mbox = new MessageBox('*');
+    MessageBox sbox = new MessageBox('/');
+    manager.register("strong message", uPen);
+    manager.register("warning box", mbox);
+    manager.register("slash box", sbox);
+
+    Product p1 = manager.create("strong message");
+    p1.use("Hello, world.");
+    Product p2 = manager.create("warning box");
+    p2.use("Hello world.");
+    Product p3 = manager.create("slash box");
+    p3.use("Hello world.");
+  }
+}
